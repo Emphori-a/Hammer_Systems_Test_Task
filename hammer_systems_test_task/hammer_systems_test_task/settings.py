e10b93fefffe55d8ffb6375591233703d137f137
Valery Martynova <emphoria@yandex.ru>
2024-04-23 11:52:16 +0300

    Correct settings

--- hammer_systems_test_task/hammer_systems_test_task/settings.py
+++ hammer_systems_test_task/hammer_systems_test_task/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.emphoria.pythonanywhere.com",
+                 "emphoria.pythonanywhere.com"]
 
 
 # Application definition
